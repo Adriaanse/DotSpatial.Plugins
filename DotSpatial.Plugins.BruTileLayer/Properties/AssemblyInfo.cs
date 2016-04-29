7e4f34fa8b6266d85b0b15eabe3519346c511285
Bart Adriaanse <bart.adriaanse@gmail.com>
2016-04-29 11:30:27 +0200

    Updated to DotSpatial release 1.9 using NuGet, to demonstrate that this works OK...

--- DotSpatial.Plugins.BruTileLayer/Properties/AssemblyInfo.cs
+++ DotSpatial.Plugins.BruTileLayer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DotSpatial.Plugins.BruTileLayer")]
-[assembly: AssemblyCopyright("Copyright © 2012-2015 FObermaier")]
+[assembly: AssemblyCopyright("Copyright © 2012-2016 FObermaier")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.0")]
-[assembly: AssemblyFileVersion("1.8.0.0")]
+[assembly: AssemblyVersion("1.9.0")]
+[assembly: AssemblyFileVersion("1.9.0.0")]
